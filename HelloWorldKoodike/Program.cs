2efeab17b52feb457efef209e41e4782b0c21b83
qerial <230301716+qerial@users.noreply.github.com>
2025-10-10 11:11:55 +0300

    init

---
+++ HelloWorldKoodike/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HelloWorldKoodike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("H");
+            Console.WriteLine("He");
+            Console.WriteLine("Hel");
+            Console.WriteLine("Hell");
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello,");
+            Console.WriteLine("Hello, W");
+            Console.WriteLine("Hello, Wo");
+            Console.WriteLine("Hello, Wor");
+            Console.WriteLine("Hello, Worl");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, Worl");
+            Console.WriteLine("Hello, Wor");
+            Console.WriteLine("Hello, Wo");
+            Console.WriteLine("Hello, W");
+            Console.WriteLine("Hello, ");
+            Console.WriteLine("Hello,");
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hell");
+            Console.WriteLine("Hel");
+            Console.WriteLine("He");
+            Console.WriteLine("H");
+        }
+    }
+}
