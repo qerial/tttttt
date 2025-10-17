9cfbe6f8781e20d84b2f887c893b0316bd7fa42f
Opilane TTHK <opilane@tthk.ee>
2025-10-17 11:12:48 +0300

    new code

---
+++ SwitchWithNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SwitchWithNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Teie töö on teha switch rakendus
+            //kus on kolm case (1, 2, 3).
+            //Iga case puhul teeb erineb arb beep-e
+            int input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("case 1");
+                    Console.Beep(1000, 500);
+                    break;
+                case 2:
+                    Console.WriteLine("case 2");
+                    Console.Beep(2000, 500);
+                    Console.Beep(2000, 500);
+                    break;
+                case 3:
+                    Console.WriteLine("case 3");
+                    Console.Beep(3000, 50);
+                    Console.Beep(3000, 50);
+                    Console.Beep(3000, 50);
+                    break;
+
+                    default:
+                    Console.WriteLine("Error kood ei tööta!");
+                    break;
+            }
+        }
+    }
+}
